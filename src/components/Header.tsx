5abd428816723af633c3f0eb74ee334abbeada34
Robertson Siu <rrsiu@uncg.edu>
2026-01-12 12:57:39 -0500

    updated navigation for contact

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,16 @@
+import { Link } from "react-router-dom";
 import "./../styles/Header.css";
 
 function Header() {
   return (
     <header>
-        <h1>T&H Laundromat</h1>
-
-        <nav>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/contact">Contact</a></li>
-            </ul>
-        </nav>
+      <h1>T&H Laundromat</h1>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
     </header>
   );
 }
